553c81ad86627e3c30a3ea46e6d43824ba9edbcb
Andre Simamora <andriansimamora@gmail.com>
2019-05-06 09:54:41 +0700

    [add] back to top button

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -14,6 +14,20 @@ export class LayoutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    window.onscroll = () => this.scroll();
+  }
+
+  scroll() {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+      document.getElementById('btnBackToTop').style.display = 'block';
+    } else {
+      document.getElementById('btnBackToTop').style.display = 'none';
+    }
+  }
+
+  toTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
   }
 
 }
