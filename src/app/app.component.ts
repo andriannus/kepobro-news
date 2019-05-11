921b9d56bfbf7221b1058fad1f69c74344204675
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2019-05-11 09:54:08 +0700

    [add] Google Analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
+declare let ga: (action: string, field: string, url?: string) => void;
+
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'kepobro-news';
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        ga('set', 'page', event.urlAfterRedirects);
+        ga('send', 'pageView');
+      }
+    });
+  }
 }
